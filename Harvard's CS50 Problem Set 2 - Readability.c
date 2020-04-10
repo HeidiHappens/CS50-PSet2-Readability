6a568f9d40721a8c05bf0cb2124f909802315cf7
HeidiHappens <62840573+HeidiHappens@users.noreply.github.com>
2020-04-10 17:14:00 +0200

    Add files via upload

--- Harvard's CS50 Problem Set 2 - Readability.c
+++ Harvard's CS50 Problem Set 2 - Readability.c
@@ -11,30 +11,30 @@ int main(void)
 //user input
     string text = get_string("Text: ");
 
-    int lettercount = 0;
-    int wordcount = 1;
-    int sentencecount = 0;
+    int letters = 0;
+    int words = 1;
+    int sentences = 0;
 
 //counts number of letters, words and sentences
     for (int i = 0, n = strlen(text); i < n; i++)
     {
         if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
         {
-            lettercount++;
+            letters++;
         }
         else if (text[i] == ' ')
         {
-            wordcount++;
+            words++;
         }
         else if (text[i] == '.' || text[i] == '?' || text[i] == '!')
         {
-            sentencecount++;
+            sentences++;
         }
     }
 
 //equates the variables L and S for the Coleman-Liau index
-    float L = (float) lettercount / (float) wordcount * 100;
-    float S = (float) sentencecount / (float) wordcount * 100;
+    float L = (float) letters / (float) words * 100;
+    float S = (float) sentences / (float) words * 100;
 
 //computes the Coleman-Liau index
     int grade = round(0.0588 * L - 0.296 * S - 15.8);
